7b558e4f47c4379bb2e9e6acca4c94ae07d64163
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-09 17:24:52 +0900

    feat: 상수 정리 및 사용할 태그 id 부여

---
+++ src/constants/dom.js
@@ -0,0 +1,8 @@
+import { $ } from '../utils/dom.js'
+
+export const $carNamesInput = $('#car-names-input')
+export const $carNamesSubmitButton = $('#car-names-submit')
+export const $racingCountInput = $('#racing-count-input')
+export const $racingCountSubmitButton = $('#racing-count-submit')
+export const $racingResult = $('#racing-result')
+export const $racingWinners = $('racing-winners')
---
+++ src/constants/message.js
@@ -0,0 +1,9 @@
+export const CAR_NAME_ERROR = {
+	LENGTH_ERROR: '자동차 이름의 길이가 올바르지 않습니다.',
+	BLANK_ERROR: '자동차 이름을 입력해 주세요',
+}
+
+export const CAR_RACE_COUNT_ERROR = {
+	TYPE_ERROR: '올바른 숫자를 입력해 주세요',
+	BLANK_ERROR: '시도할 횟수를 입력해 주세요',
+}
