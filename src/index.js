52118dd65fff597776db1f88d2da3f786bed2485
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-09 23:00:04 +0900

    feat: 자동차 이름 input, button disable 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { $app, $carNamesInput } from './constants/dom.js'
+import { $app, $carNamesInput, $carNamesSubmitButton } from './constants/dom.js'
 import { MIN_CAR_NAME_LENGTH, MAX_CAR_NAME_LENGTH } from './constants/condition.js'
 import { CAR_NAME_ERROR } from './constants/message.js'
+import { disableDOM } from './utils/dom.js'
 import Car from './car.js'
 
 class RacingGame {
@@ -11,18 +12,24 @@ class RacingGame {
 
 	initEventListeners() {
 		$app.addEventListener('click', e => {
+			e.preventDefault()
 			if (e.target.id === 'car-names-submit') {
-				e.preventDefault()
-				const carNames = $carNamesInput.value.trim().split(',')
-				if (!this.isValidCarNames(carNames)) {
-					alert(CAR_NAME_ERROR)
-					return
-				}
-				this.cars = carNames.map(carName => new Car(carName))
+				this.handleSubmitCarNames()
 			}
 		})
 	}
 
+	handleSubmitCarNames() {
+		const carNames = $carNamesInput.value.trim().split(',')
+		if (!this.isValidCarNames(carNames)) {
+			alert(CAR_NAME_ERROR)
+			return
+		}
+		this.cars = carNames.map(carName => new Car(carName))
+
+		disableDOM($carNamesInput)
+		disableDOM($carNamesSubmitButton)
+	}
 	isValidCarNames(carNames) {
 		return carNames.every(
 			carName => MIN_CAR_NAME_LENGTH <= carName.length && carName.length <= MAX_CAR_NAME_LENGTH
