1cc838183bf72e4a8500ab8dae36d0fc8543e432
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-15 22:44:36 +0900

    refactor: 자동차 이름 유효성검사 로직 개선

--- src/constants/condition.js
+++ src/constants/condition.js
@@ -1,4 +1,7 @@
 export const MIN_CAR_NAME_LENGTH = 1
 export const MAX_CAR_NAME_LENGTH = 5
+export const MIN_CAR_COUNT = 2
+
 export const MIN_RACING_COUNT = 1
+
 export const CAR_MOVES_CONDITION_NUMBER = 4
--- src/constants/message.js
+++ src/constants/message.js
@@ -1,4 +1,8 @@
-import { MAX_CAR_NAME_LENGTH } from './condition.js'
+import { MIN_CAR_COUNT } from './condition.js'
 
-export const CAR_NAME_ERROR = `자동차 이름을 ${MAX_CAR_NAME_LENGTH}자 이하로 콤마로 구분하여 입력해주세요`
-export const CAR_RACE_COUNT_ERROR = '올바른 숫자를 입력해 주세요'
+export const ERR_MSG = {
+	invalidCarCount: `자동차 이름을 최대 ${MIN_CAR_COUNT}개 이상 입력해주세요.`,
+	invalidCarNameLength: `자동차 이름을 5자 이하로 콤마로 구분하여 입력해주세요`,
+	repeatedCarName: ` 자동차 이름을 중복 없이 입력해 주세요`,
+	invalidRaceCount: '올바른 숫자를 입력해 주세요',
+}
